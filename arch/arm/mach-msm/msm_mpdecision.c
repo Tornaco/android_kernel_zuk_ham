d4a31ff49c9c0748baba0b840c037ee6b4314c0d
Anand Godbole <andy@AnandGodbole.(none)>
2016-11-11 09:58:08 +0530

    Grow Baby Grow :D MPDECISION

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -32,10 +32,11 @@
 #include <linux/hrtimer.h>
 #include <linux/delay.h>
 
-#define MPDEC_TAG "[MPDEC]: "
-#define MSM_MPDEC_STARTDELAY 60000
-#define MSM_MPDEC_DELAY 500
-#define MSM_MPDEC_PAUSE 10000
+#define MPDEC_TAG                       "[MPDEC]: "
+#define MSM_MPDEC_STARTDELAY            70000
+#define MSM_MPDEC_DELAY                 500
+#define MSM_MPDEC_PAUSE                 10000
+#define MSM_MPDEC_IDLE_FREQ             486000
 
 enum {
 	MSM_MPDEC_DISABLED = 0,
@@ -60,17 +61,22 @@ static struct msm_mpdec_tuners {
 	unsigned int delay;
 	unsigned int pause;
 	bool scroff_single_core;
+	unsigned long int idle_freq;
 } msm_mpdec_tuners_ins = {
 	.startdelay = MSM_MPDEC_STARTDELAY,
 	.delay = MSM_MPDEC_DELAY,
 	.pause = MSM_MPDEC_PAUSE,
 	.scroff_single_core = true,
+	.idle_freq = MSM_MPDEC_IDLE_FREQ,
 };
 
-static unsigned int NwNs_Threshold[4] = {25, 0, 0, 5};
+static unsigned int NwNs_Threshold[4] = {35, 0, 0, 5};
 static unsigned int TwTs_Threshold[4] = {250, 0, 0, 250};
 
 extern unsigned int get_rq_info(void);
+extern unsigned long acpuclk_8x60_get_rate(int);
+
+unsigned int state = MSM_MPDEC_IDLE;
 bool was_paused = false;
 
 static int mp_decision(void)
@@ -84,6 +90,9 @@ static int mp_decision(void)
 	static cputime64_t last_time;
 	cputime64_t current_time;
 	cputime64_t this_time = 0;
+
+	if (state == MSM_MPDEC_DISABLED)
+		return MSM_MPDEC_DISABLED;
 
 	current_time = ktime_to_ms(ktime_get());
 	if (current_time <= msm_mpdec_tuners_ins.startdelay)
@@ -128,7 +137,6 @@ static int mp_decision(void)
 
 static void msm_mpdec_work_thread(struct work_struct *work)
 {
-	int ret = 0;
 	unsigned int cpu = nr_cpu_ids;
 	cputime64_t on_time = 0;
 
@@ -149,22 +157,25 @@ static void msm_mpdec_work_thread(struct work_struct *work)
 		was_paused = false;
 	}
 
-	ret = mp_decision();
-	switch (ret) {
+	state = mp_decision();
+	switch (state) {
 	case MSM_MPDEC_DISABLED:
 	case MSM_MPDEC_IDLE:
 		break;
 	case MSM_MPDEC_DOWN:
 		cpu = (CONFIG_NR_CPUS - 1);
 		if (cpu < nr_cpu_ids) {
+			if (cpu_online(cpu))
+				if (acpuclk_8x60_get_rate(cpu) > msm_mpdec_tuners_ins.idle_freq)
+					break;
 			if ((per_cpu(msm_mpdec_cpudata, cpu).online == true) && (cpu_online(cpu))) {
 				cpu_down(cpu);
 				per_cpu(msm_mpdec_cpudata, cpu).online = false;
 				on_time = ktime_to_ms(ktime_get()) - per_cpu(msm_mpdec_cpudata, cpu).on_time;
 				pr_info(MPDEC_TAG"CPU[%d] on->off | Mask=[%d%d] | time online: %llu\n",
 						cpu, cpu_online(0), cpu_online(1), on_time);
-			} else if ((per_cpu(msm_mpdec_cpudata, cpu).online == true) && (!cpu_online(cpu))) {
-				pr_info(MPDEC_TAG"CPU[%d] was unplugged outside of mpdecision! | pausing [%d]ms\n",
+			} else if (per_cpu(msm_mpdec_cpudata, cpu).online != cpu_online(cpu)) {
+				pr_info(MPDEC_TAG"CPU[%d] was controlled outside of mpdecision! | pausing [%d]ms\n",
 						cpu, msm_mpdec_tuners_ins.pause);
 				msleep(msm_mpdec_tuners_ins.pause);
 				was_paused = true;
@@ -180,8 +191,8 @@ static void msm_mpdec_work_thread(struct work_struct *work)
 				per_cpu(msm_mpdec_cpudata, cpu).on_time = ktime_to_ms(ktime_get());
 				pr_info(MPDEC_TAG"CPU[%d] off->on | Mask=[%d%d]\n",
 						cpu, cpu_online(0), cpu_online(1));
-			} else if ((per_cpu(msm_mpdec_cpudata, cpu).online == false) && (cpu_online(cpu))) {
-				pr_info(MPDEC_TAG"CPU[%d] was hotplugged outside of mpdecision! | pausing [%d]ms\n",
+			} else if (per_cpu(msm_mpdec_cpudata, cpu).online != cpu_online(cpu)) {
+				pr_info(MPDEC_TAG"CPU[%d] was controlled outside of mpdecision! | pausing [%d]ms\n",
 						cpu, msm_mpdec_tuners_ins.pause);
 				msleep(msm_mpdec_tuners_ins.pause);
 				was_paused = true;
@@ -190,13 +201,14 @@ static void msm_mpdec_work_thread(struct work_struct *work)
 		break;
 	default:
 		pr_err(MPDEC_TAG"%s: invalid mpdec hotplug state %d\n",
-		       __func__, ret);
+		       __func__, state);
 	}
 	mutex_unlock(&msm_cpu_lock);
 
 out:
-	schedule_delayed_work(&msm_mpdec_work,
-			msecs_to_jiffies(msm_mpdec_tuners_ins.delay));
+	if (state != MSM_MPDEC_DISABLED)
+		schedule_delayed_work(&msm_mpdec_work,
+				msecs_to_jiffies(msm_mpdec_tuners_ins.delay));
 	return;
 }
 
@@ -256,6 +268,31 @@ show_one(startdelay, startdelay);
 show_one(delay, delay);
 show_one(pause, pause);
 show_one(scroff_single_core, scroff_single_core);
+
+static ssize_t show_idle_freq (struct kobject *kobj, struct attribute *attr,
+                                   char *buf)
+{
+	return sprintf(buf, "%lu\n", msm_mpdec_tuners_ins.idle_freq);
+}
+
+static ssize_t show_enabled(struct kobject *a, struct attribute *b,
+				   char *buf)
+{
+	unsigned int enabled;
+	switch (state) {
+	case MSM_MPDEC_DISABLED:
+		enabled = 0;
+		break;
+	case MSM_MPDEC_IDLE:
+	case MSM_MPDEC_DOWN:
+	case MSM_MPDEC_UP:
+		enabled = 1;
+		break;
+	default:
+		enabled = 333;
+	}
+	return sprintf(buf, "%u\n", enabled);
+}
 
 static ssize_t show_nwns_threshold_up(struct kobject *kobj, struct attribute *attr,
 					char *buf)
@@ -323,6 +360,19 @@ static ssize_t store_pause(struct kobject *a, struct attribute *b,
 	return count;
 }
 
+static ssize_t store_idle_freq(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
+{
+	long unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%lu", &input);
+	if (ret != 1)
+		return -EINVAL;
+	msm_mpdec_tuners_ins.idle_freq = acpu_check_khz_value(input);
+
+	return count;
+}
+
 static ssize_t store_scroff_single_core(struct kobject *a, struct attribute *b,
 				   const char *buf, size_t count)
 {
@@ -337,6 +387,57 @@ static ssize_t store_scroff_single_core(struct kobject *a, struct attribute *b,
 		break;
 	case '1':
 		msm_mpdec_tuners_ins.scroff_single_core = input;
+		break;
+	default:
+		ret = -EINVAL;
+	}
+	return count;
+}
+
+static ssize_t store_enabled(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
+{
+	unsigned int cpu, input, enabled;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+	if (ret != 1)
+		return -EINVAL;
+
+	switch (state) {
+	case MSM_MPDEC_DISABLED:
+		enabled = 0;
+		break;
+	case MSM_MPDEC_IDLE:
+	case MSM_MPDEC_DOWN:
+	case MSM_MPDEC_UP:
+		enabled = 1;
+		break;
+	default:
+		enabled = 333;
+	}
+
+	if (buf[0] == enabled)
+		return -EINVAL;
+
+	switch (buf[0]) {
+	case '0':
+		state = MSM_MPDEC_DISABLED;
+		cpu = (CONFIG_NR_CPUS - 1);
+		if (!cpu_online(cpu)) {
+			per_cpu(msm_mpdec_cpudata, cpu).on_time = ktime_to_ms(ktime_get());
+			per_cpu(msm_mpdec_cpudata, cpu).online = true;
+			cpu_up(cpu);
+			pr_info(MPDEC_TAG"nap time... Hot plugged CPU[%d] | Mask=[%d%d]\n",
+					 cpu, cpu_online(0), cpu_online(1));
+		} else {
+			pr_info(MPDEC_TAG"nap time...\n");
+		}
+		break;
+	case '1':
+		state = MSM_MPDEC_IDLE;
+		was_paused = true;
+		schedule_delayed_work(&msm_mpdec_work, 0);
+		pr_info(MPDEC_TAG"firing up mpdecision...\n");
 		break;
 	default:
 		ret = -EINVAL;
@@ -404,6 +505,8 @@ define_one_global_rw(startdelay);
 define_one_global_rw(delay);
 define_one_global_rw(pause);
 define_one_global_rw(scroff_single_core);
+define_one_global_rw(idle_freq);
+define_one_global_rw(enabled);
 define_one_global_rw(nwns_threshold_up);
 define_one_global_rw(nwns_threshold_down);
 define_one_global_rw(twts_threshold_up);
@@ -414,6 +517,8 @@ static struct attribute *msm_mpdec_attributes[] = {
 	&delay.attr,
 	&pause.attr,
 	&scroff_single_core.attr,
+	&idle_freq.attr,
+	&enabled.attr,
 	&nwns_threshold_up.attr,
 	&nwns_threshold_down.attr,
 	&twts_threshold_up.attr,
@@ -439,7 +544,8 @@ static int __init msm_mpdec(void)
 	}
 
 	INIT_DELAYED_WORK(&msm_mpdec_work, msm_mpdec_work_thread);
-	schedule_delayed_work(&msm_mpdec_work, 0);
+	if (state != MSM_MPDEC_DISABLED)
+		schedule_delayed_work(&msm_mpdec_work, 0);
 
 	register_early_suspend(&msm_mpdec_early_suspend_handler);
 
